f4427b4658a44cf22abcd108cdb9cdb0ffaf8a9d
mnee <mnee@stanford.edu>
2017-08-01 15:09:36 -0700

    Switched to prod

--- signup.js
+++ signup.js
@@ -3,7 +3,7 @@
 // Mixpanel init
 (function(e,a){if(!a.__SV){var b=window;try{var c,l,i,j=b.location,g=j.hash;c=function(a,b){return(l=a.match(RegExp(b+"=([^&]*)")))?l[1]:null};g&&c(g,"state")&&(i=JSON.parse(decodeURIComponent(c(g,"state"))),"mpeditor"===i.action&&(b.sessionStorage.setItem("_mpcehash",g),history.replaceState(i.desiredHash||"",e.title,j.pathname+j.search)))}catch(m){}var k,h;window.mixpanel=a;a._i=[];a.init=function(b,c,f){function e(b,a){var c=a.split(".");2==c.length&&(b=b[c[0]],a=c[1]);b[a]=function(){b.push([a].concat(Array.prototype.slice.call(arguments, 0)))}}var d=a;"undefined"!==typeof f?d=a[f]=[]:f="mixpanel";d.people=d.people||[];d.toString=function(b){var a="mixpanel";"mixpanel"!==f&&(a+="."+f);b||(a+=" (stub)");return a};d.people.toString=function(){return d.toString(1)+".people (stub)"};k="disable time_event track track_pageview track_links track_forms register register_once alias unregister identify name_tag set_config reset people.set people.set_once people.increment people.append people.union people.track_charge people.clear_charges people.delete_user".split(" "); for(h=0;h<k.length;h++)e(d,k[h]);a._i.push([b,c,f])};a.__SV=1.2;b=e.createElement("script");b.type="text/javascript";b.async=!0;b.src="undefined"!==typeof MIXPANEL_CUSTOM_LIB_URL?MIXPANEL_CUSTOM_LIB_URL:"file:"===e.location.protocol&&"//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js".match(/^\/\//)?"https://cdn.mxpnl.com/libs/mixpanel-2-latest.min.js":"//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js";c=e.getElementsByTagName("script")[0];c.parentNode.insertBefore(b,c)}})(document,window.mixpanel||[]);
 
-mixpanel.init("99f35bfccecaa88d8b1da3f4cf850408");
+mixpanel.init("d88a3f958bee96f47745ae7b6f280df9");
         
 window.onmessage = function(e){
     mixpanel.identify(e.data);
@@ -11,7 +11,7 @@ window.onmessage = function(e){
         
 function handleToken(data) {
     console.log(data.data.session.access_token);
-    window.top.location.href = "https://stg-parent.zeal.com/#!/activation/name/?accessToken=" + data.data.session.access_token;
+    window.top.location.href = "https://parent.zeal.com/#!/activation/name/?accessToken=" + data.data.session.access_token;
 }
         
 function signup() {
@@ -19,12 +19,15 @@ function signup() {
     var password = document.getElementById("password").value;
     var prof = {"user":{"name": email, "password": password}};
     console.log(JSON.stringify(prof));
-    mixpanel.track("stg_landing_signup");
-    $.post("https://stg-api.zeal.com/v7/public/users/parent", prof).done(handleToken);
+    mixpanel.track("landing_signup");
+    $.post("https://api.zeal.com/v7/public/users/parent", prof).done(handleToken);
 }
 
 window.setInterval(checkPassword,500);
-        
+    
+var passMix = true;
+var emailMix = true;
+
 function checkPassword() {
     // 8 chars, 1 Let, 1 Num
     var curPass = document.getElementById("password").value;
@@ -51,8 +54,16 @@ function checkPassword() {
 
     var button = document.getElementById("signup_button");
     if (length && letter && num) {
+        if (passMix) {
+            mixpanel.track("entered_password");
+            passMix = false;
+        }
         document.getElementById("password_help").style.visibility="hidden";
         if (email) {
+            if (emailMix) {
+                mixpanel.track("entered_email");
+                emailMix = false;
+            }
             button.style.pointerEvents="all";
             button.style.backgroundColor="rgb(19,178,197)";
         }
@@ -67,11 +78,6 @@ function checkPassword() {
         
 function showPassHelp() {
     document.getElementById("password_help").style.visibility="visible";
-    mixpanel.track("stg_clicked_password_enter");
-}
-         
-function clickEmailField() {
-    mixpanel.track("stg_clicked_email_field");
 }
 
 var click1 = 0;
